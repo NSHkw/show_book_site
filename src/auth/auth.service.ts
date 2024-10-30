17b0cd007c4281f28942fa6d2a35e19d629051a8
NSHkw <64311828+NSHkw@users.noreply.github.com>
2024-10-30 16:19:13 +0900

    add: bcrypt signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { DEFAULT_CUSTOMER_POINT } from 'src/constants/point.constant';
+import { ConfigService } from '@nestjs/config';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
   async signup({ email, password, passwordConfirm, nickname }) {
     const isPasswordMatched = password === passwordConfirm;
@@ -20,9 +25,13 @@ export class AuthService {
       throw new BadRequestException('이미 존재하는 이메일');
     }
 
+    const hashRound = this.configService.get<number>('PASSWORD_HASH_ROUND');
+
+    const hashedPassword = bcrypt.hashSync(password, hashRound);
+
     const user = await this.userRepository.save({
       email,
-      password,
+      password: hashedPassword,
       nickname,
       points: DEFAULT_CUSTOMER_POINT,
     });
